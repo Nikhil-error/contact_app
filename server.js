13bac11f32debb324423ad187d85585db57c9952
Nikhil-error <nikhilgupta12.3.2004@gmail.com>
2025-12-14 23:09:47 +0530

    f

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+app.use('/api/contact', require('./routes/contactRoutes'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
