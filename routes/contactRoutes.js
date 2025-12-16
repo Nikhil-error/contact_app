fb627dde3a9b4b82c68ef4e3c5ff90e698f1663c
Nikhil-error <nikhilgupta12.3.2004@gmail.com>
2025-12-17 00:32:57 +0530

    refine

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+// import { getContact } from '../controllers/contactController';
+const { getContact,createContact,updateContact,deleteContact } = require('../controllers/contactController');
 
-router.route("/").get((req, res) => {
-    res.send('Hello World!');
-});
+
+router.route("/").get(getContact);
+router.route("/").post(createContact);
+router.route("/:id").put(updateContact);
+router.route("/:id").delete(deleteContact);
 
 module.exports = router;
